50e4619fc996559c8b4a793eeb7d221d516b6390
whohet <whohet@gmail.com>
2023-08-04 15:27:55 +0530

    project-init-3

---
+++ middleware/userAuth.js
@@ -0,0 +1,12 @@
+const userAuth = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      next();
+    } else {
+      return res.status(401).json({
+        success: false,
+        message: "Access denied. Please login to continue.",
+      });
+    }
+  };
+  
+  module.exports = userAuth;
