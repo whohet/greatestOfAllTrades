21c43e5407b01412a6955c42960af8cae8c3a557
Ishita <mittalishita31@gmail.com>
2023-08-05 00:36:52 +0530

    ui of login, signup page for user

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import LoginUser from './components/LoginUser'
 
 function App() {
   return (
     <div className="App">
-      
+      <LoginUser></LoginUser>
     </div>
   );
 }
