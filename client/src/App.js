c1a82eb2f9c1b97c646d294f088978ec28922a51
Jonnadula Venkata Sai Tanish <venkatasaitanish@gmail.com>
2023-08-08 14:49:56 +0530

    frontend-auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import SignUp from './components/Auth/SignUp/SignUp';
+import LoginUser from './components/Auth/Login/LoginUser';
+import Header from "./components/Header/Header";
+import Dashboard from "./components/Dashboard/Dashboard"
+
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/login" element={<LoginUser />}/>
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ client/src/components/Auth/SignUp/SignUp.jsx
@@ -0,0 +1,167 @@
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+// import FormControlLabel from '@mui/material/FormControlLabel';
+// import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+import { useState } from 'react';
+import {useNavigate} from "react-router-dom";
+import useToast from "../../../customHooks/useToast/useToast";
+import 'react-toastify/dist/ReactToastify.css';
+import { registerAPI } from "../../../api/userAuth";
+
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        GOAT
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+// TODO remove, this demo shouldn't need to reset the theme.
+
+const defaultTheme = createTheme();
+
+export default function SignUp() {
+
+
+  const [signUpEmail, setSignUpEmail] = useState("");
+  const [signUpUsername, setSingUpUsername] = useState("");
+  const [signUpPassword, setSignUpPassword] = useState("");
+  const [signUpConfirmPassword, setSignUpConfirmPassword] = useState("");
+  const [toast, ToastContainer] = useToast();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const userData = {
+        email: signUpEmail,
+        username: signUpUsername,
+        password: signUpPassword,
+        confirmPassword: signUpConfirmPassword,
+        role: "user"
+      };
+      await registerAPI(userData);
+      toast.success(
+        "Account Registered successfully."
+      );
+    } catch (err) {
+      if (err && err.response && err.response.data) {
+        toast.error(err.response.data.message);
+      } else {
+        toast.error("Internal server error. Please try again.");
+      }
+    }
+  };
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField
+                  autoComplete="given-name"
+                  name="username"
+                  required
+                  fullWidth
+                  id="username"
+                  label="Name"
+                  autoFocus
+                  onChange={(e) => setSingUpUsername(e.target.value)}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                  onChange={(e) => setSignUpEmail(e.target.value)}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                  onChange={(e) => setSignUpPassword(e.target.value)}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="confirmPassword"
+                  label="Confirm Password"
+                  type="password"
+                  id="confirmPassword"
+                  autoComplete="new-password"
+                  onChange={(e) => setSignUpConfirmPassword(e.target.value)}
+                />
+              </Grid>
+              {/* <Grid item xs={12}>
+                <FormControlLabel
+                  control={<Checkbox value="allowExtraEmails" color="primary" />}
+                  label="I want to receive inspiration, marketing promotions and updates via email."
+                />
+              </Grid> */}
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign Up
+            </Button>
+            <Grid container justifyContent="center">
+              <Grid item>
+                <Link href="/login" variant="body2">
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 5 }} />
+      </Container>
+      {ToastContainer}
+    </ThemeProvider>
+  );
+}
